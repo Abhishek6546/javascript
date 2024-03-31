7afbf4d7a3ccd255d47c3c5b7f47bf8abb6b6fb1
Abhishek6546 <abhishekk11603@gmail.com>
2024-03-31 12:28:42 +0530

    json

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -28,12 +28,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(0, 3)
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(0, 3)
 console.log("C ", myArr);
 console.log(myn2);
